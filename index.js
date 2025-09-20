c0074e9bbfba0b87086925df325a0836aa6881a3
312515032 <145183725+312515032@users.noreply.github.com>
2025-09-21 02:12:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+import { simonHelper } from "./modules/simon-article-helper.js";
+import { generate } from "./modules/template-helper.js";
+
+const configs = {
+  startPage: 1,
+  endPage: 5,     // 先小一點測
+  filter: "",     // 先不要過濾，OK 再加
+
+  // 直接指定分類 URL
+  pages: [
+    { fileName: "reading.html",        pageName: "Reading",            categoryUrl: "https://ielts-simon.study/ielts-reading/" },
+    { fileName: "listening.html",      pageName: "Listening",          categoryUrl: "https://ielts-simon.study/ielts-listening/" },
+    { fileName: "speaking.html",       pageName: "Speaking",           categoryUrl: "https://ielts-simon.study/ielts-speaking/" },
+    { fileName: "writing-task-1.html", pageName: "Writing Task 1 (Ac)",categoryUrl: "https://ielts-simon.study/ielts-writing-task-1-academic/" },
+    { fileName: "index.html",          pageName: "Writing Task 2",     categoryUrl: "https://ielts-simon.study/ielts-writing-task-2/" },
+  ],
+};
+
+const pages = await simonHelper.getPages(configs);
+pages.forEach((page) => generate(page, configs));
+
+
+
+
+/*
 // 入口：只負責呼叫抓文 + 產生頁面
 import { simonHelper } from "./modules/simon-article-helper.js";
 import { generate } from "./modules/template-helper.js";
@@ -25,4 +50,4 @@ const configs = {
 
 const pages = await simonHelper.getPages(configs);
 pages.forEach((page) => generate(page, configs));
-
+*/
